c24f4610bee63547ca5b17adc38e51d21682f0ac
masoncm97 <masoncmathew@gmail.com>
2022-09-29 10:39:07 -0700

    Renamed working dir

--- masonmathai/src/common/functions.js
+++
@@ -1,7 +0,0 @@
-const styles = {
-    toggleElement: (x1, x2)  => ((x2 === undefined) ?
-    { display: x1 ? 'flex' : 'none'} : 
-    { display: x1 && x2 ? 'flex' : 'none' })
-};
-
-export default styles;
